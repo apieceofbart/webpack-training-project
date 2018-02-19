02d5416538101d92768180a4dd8d3c568554052b
Bartek Kozera <bartosz.kozera@gmail.com>
2018-02-19 17:32:48 +0100

    add svg-url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,14 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        loader: 'file-loader',
+        loader: 'svg-url-loader',
+        options: {
+          // Inline files smaller than 10 kB (10240 bytes)
+          limit: 10 * 1024,
+          // Remove the quotes from the url
+          // (they’re unnecessary in most cases)
+          noquotes: true,
+        },
       },
     ],
   },
